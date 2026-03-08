798a4b83e3605bd7c3998b8c028a2477f0fdf15c
Menna-Baligh <mennabaligh55520@yahoo.com>
2026-03-08 17:20:06 +0200

    add PlanSeeder and update plans migration to include features and rename summaries_limit

---
+++ database/seeders/PlanSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Plan;
+use Illuminate\Database\Seeder;
+
+class PlanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $plans = [
+            [
+                "name" => "Basic",
+                'price' => 2400.00,
+                'summaries_limit' => 270,
+                'duration_days' => 30,
+                'features' => json_encode([
+                    'Key Important Information',
+                    'Comparative Analysis'
+                ])
+            ],
+            [
+                "name" => "Pro",
+                'price' => 6000.00,
+                'summaries_limit' => 450,
+                'duration_days' => 30,
+                'features' => json_encode([
+                    'Key Important Information',
+                    'Comparative Analysis',
+                    'Decision Support'
+                ])
+            ],
+            [
+                "name" => "Premium",
+                'price' => 12000.00,
+                'summaries_limit' => 660,
+                'duration_days' => 30,
+                'features' => json_encode([
+                    'Key Important Information',
+                    'Comparative Analysis',
+                    'Decision Support',
+                    'DiagnoBot'
+                ])
+            ],
+        ];
+        foreach ($plans as $plan) {
+            Plan::create($plan);
+        }
+    }
+}
