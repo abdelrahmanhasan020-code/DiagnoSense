dcc6a69eb8af82700f13a42cbf6649d073274ac6
abdelrahmanhasan020-code <abdelrahmanhasan020@gmail.com>
2026-03-22 18:17:06 +0200

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->nullable();
-            $table->string('phone')->nullable();
+            $table->string('email')->nullable(false);
+            $table->string('phone')->nullable(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('type', ['doctor', 'patient']);
