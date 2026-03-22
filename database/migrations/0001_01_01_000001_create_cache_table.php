24206fbd7a0de0579e68ffe80dc4f6f3aa44dd87
abdelrahmanhasan020-code <abdelrahmanhasan020@gmail.com>
2026-03-22 19:54:53 +0200

    Update 0001_01_01_000001_create_cache_table.php

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -6,23 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-Schema::create('cache', function (Blueprint $table) {
-    $table->id();
-    $table->string('cache_key');
-    $table->mediumText('value');
-    $table->integer('expiration');
-});
+        Schema::create('cache', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->mediumText('value');
+            $table->integer('expiration');
+        });
 
-Schema::create('cache_locks', function (Blueprint $table) {
-    $table->id();
-    $table->string('cache_key');
-    $table->string('owner');
-    $table->integer('expiration');
-});
+        Schema::create('cache_locks', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->string('owner');
+            $table->integer('expiration');
+        });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('cache');
