4ddd2881358b4fe6d904ca39824ae562a9288943
abdelrahmanhasan020-code <abdelrahmanhasan020@gmail.com>
2026-03-22 18:47:51 +0200

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -30,14 +30,15 @@ return new class extends Migration
             $table->timestamp('created_at')->nullable();
         });
 
+ 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
-        });
+          });
     }
 
     /**
